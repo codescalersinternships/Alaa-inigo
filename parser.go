ae9bb04ab090658a77b1128331db8e61fe57cdb6
AlaaElattar <alaamahmoud.1223@gmail.com>
2022-07-19 06:35:48 +0200

    Parser v1.5

---
+++ parser.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Parser struct { //nested maps
+	nested_map map[string]map[string]string
+}
+
+func (parser *Parser) LoadFromFile(path string) (err error) {
+	content, err := ioutil.ReadFile(path)
+	msg := string(content)
+	if err != nil {
+		panic(err)
+	}
+	parser.nested_map, err = Parse_ini(msg)
+	return err
+}
+
+func (parser *Parser) LoadFromText(txt string) (err error) {
+	if len(txt) != 0 {
+		parser.nested_map, err = Parse_ini(txt)
+	} else {
+		panic(err)
+	}
+
+	return err
+
+}
+
+func Parse_ini(txt string) (map[string]map[string]string, error) {
+	ini := make(map[string]map[string]string)
+	var head string
+
+	sectionHead := regexp.MustCompile(`^\[([^]]*)\]\s*$`)
+	keyValue := regexp.MustCompile(`^(\w*)\s*=\s*(.*?)\s*$`)
+	reader := bufio.NewReader(strings.NewReader(txt))
+
+	for {
+		line, _ := reader.ReadString('\n')
+		result := sectionHead.FindStringSubmatch(line)
+		if len(result) > 0 {
+			head = result[1]
+			ini[head] = make(map[string]string)
+			continue
+		}
+
+		result = keyValue.FindStringSubmatch(line)
+		if len(result) > 0 {
+			key, value := result[1], result[2]
+			ini[head][key] = value
+			continue
+		}
+		if line == "" {
+			break
+		}
+	}
+	fmt.Println(ini)
+	return ini, nil
+
+}
+
+func (parser *Parser) GetSections() map[string]map[string]string {
+	return parser.nested_map
+
+}
+
+func (parser *Parser) GetSectionsName() {
+
+	for sectionName := range parser.nested_map {
+		fmt.Println(sectionName)
+	}
+}
+
+func (parser *Parser) Set(name string, key string, value string) map[string]string {
+	section := parser.nested_map[name]
+	section[key] = value
+	fmt.Println(section)
+	return section
+}
+
+func (parser *Parser) GetKeys(sectionName string) error {
+	section := parser.nested_map[sectionName]
+	if section != nil {
+		for key, value := range section {
+			fmt.Println(key, value)
+		}
+
+	} else {
+		return errors.New("No Section with that name !!")
+	}
+
+	return nil
+
+}
+
+func (parser *Parser) LoadToFile() (err error) {
+	file, err := os.Create("output.ini")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := ""
+	for section, keys := range parser.nested_map {
+		data += "[" + section + "]\n"
+		for key, value := range keys {
+			data += key + " = " + value + "\n"
+		}
+
+	}
+
+	file.WriteString(data)
+	file.Close()
+	return err
+
+}
+
+func main() {
+
+	parser := Parser{}
+
+	parser.LoadFromFile("/home/aya/codescalers/parser_ini/PHP.ini")
+
+	fmt.Println(parser.GetSections())
+
+	parser.GetSectionsName()
+
+	parser.Set("owner", "name", "alaa")
+
+	parser.GetKeys("database")
+
+	parser.LoadToFile()
+
+}
---
+++ parser_test.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParseIni(t *testing.T) {
+
+	text := "; last modified 1 April 2001 by John Doe\n" +
+		"[owner]\n" + "name = John Doe\n" + "organization = Acme Widgets Inc.\n" +
+		"\n" + "[database]\n" + "; use IP address in case network name resolution is not working\n" +
+		"server = 192.0.2.62\n" + "port = 143\n" + "file = payroll.dat\n"
+
+	actual, err := Parse_ini(text)
+
+	if err != nil {
+		t.Error(fmt.Sprintf("Error in parsing: '%v'", err))
+	}
+	expected := make(map[string]map[string]string)
+	expected["owner"] = make(map[string]string)
+	expected["owner"]["name"] = "John Doe"
+	expected["owner"]["organization"] = "Acme Widgets Inc."
+	expected["database"] = make(map[string]string)
+	expected["database"]["server"] = "192.0.2.62"
+	expected["database"]["port"] = "143"
+	expected["database"]["file"] = "payroll.dat"
+	if !compareMapMap(actual, expected) {
+		t.Error(fmt.Sprintf("Expected : '%v', Actual : '%v'", expected, actual))
+	}
+}
+
+func TestGetSections(t *testing.T) {
+	parser := Parser{
+		map[string]map[string]string{
+			"owner": {
+				"name":         "John Doe",
+				"organization": "Acme Widgets Inc.",
+			},
+			"database": {
+				"file":   "payroll.dat",
+				"port":   "143",
+				"server": "129.0.2.62",
+			},
+		},
+	}
+	actual := parser.GetSections()
+
+	expected := map[string]map[string]string{
+		"database": {
+			"file":   "payroll.dat",
+			"port":   "143",
+			"server": "129.0.2.62",
+		},
+		"owner": {
+			"name":         "John Doe",
+			"organization": "Acme Widgets Inc.",
+		},
+	}
+
+	if !compareMapMap(actual, expected) {
+		t.Error(fmt.Sprintf("Expected '%v', Actual '%v'", expected, actual))
+	}
+}
+
+func TestSet(t *testing.T) {
+	parser := Parser{
+		map[string]map[string]string{
+			"owner": {
+				"name":         "John Doe",
+				"organization": "Acme Widgets Inc.",
+			},
+			"database": {
+				"file":   "payroll.dat",
+				"port":   "143",
+				"server": "192.0.2.62",
+			},
+		},
+	}
+
+	expected := map[string]string{
+		"file":   "payroll.dat",
+		"port":   "500",
+		"server": "192.0.2.62",
+	}
+
+	actual := parser.Set("database", "port", "500")
+
+	if !compareMap(actual, expected) {
+		t.Error(fmt.Sprintf("Expected '%v', Actual '%v'", expected, actual))
+	}
+
+}
+
+func TestGetKeys(t *testing.T) {
+
+	parser := Parser{
+		map[string]map[string]string{
+			"owner": {
+				"name":         "John Doe",
+				"organization": "Acme Widgets Inc.",
+			},
+			"database": {
+				"file":   "payroll.dat",
+				"port":   "143",
+				"server": "192.0.2.62",
+			},
+		},
+	}
+
+	expected := map[string]string{
+		"server": "192.0.2.62",
+		"port":   "143",
+		"file":   "payroll.dat",
+	}
+
+	err := parser.GetKeys("database")
+
+	if err != nil {
+		t.Error(fmt.Sprintf("Expected '%v' ", expected))
+	}
+
+}
+
+func compareMapMap(a, b map[string]map[string]string) bool {
+
+	if len(a) != len(b) {
+		return false
+	}
+	for key, value := range a {
+		if !compareMap(value, b[key]) {
+			return false
+		}
+	}
+	for key, value := range b {
+		if !compareMap(value, a[key]) {
+			return false
+		}
+	}
+	return true
+}
+
+func compareMap(a, b map[string]string) bool {
+
+	if len(a) != len(b) {
+		return false
+	}
+	for key, value := range a {
+		if value != b[key] {
+			return false
+		}
+	}
+	for key, value := range b {
+		if value != a[key] {
+			return false
+		}
+	}
+	return true
+}
